4f3ea988603965edb043f958dc577a8b115cba93
lekosk2001 <lekosk2001@naver.com>
2024-09-02 22:37:53 +0900

    feat : create exeption

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+    ConflictException,
+    Injectable,
+    InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './auth.entity';
 import { Repository } from 'typeorm';
@@ -20,7 +24,16 @@ export class AuthService {
             username,
             password,
         });
-        await this.userRepository.save(user);
+
+        try {
+            await this.userRepository.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('있는 유저네임');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
 
         return user;
     }
